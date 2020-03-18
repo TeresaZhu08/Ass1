1abcef4acd2770b7ac09c626197cbd2b24edf136
teresa <teresa.zhu0815@gmail.com>
2020-03-19 12:30:48 +1300

    new

--- WebDev_Assignment1/urls.py
+++ WebDev_Assignment1/urls.py
@@ -1,4 +1,4 @@
-"""Assignment1 URL Configuration
+"""WebDev_Assignment1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
