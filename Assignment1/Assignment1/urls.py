53778557511d4c06665d9f15f88be96f1aeee76f
teresa <teresa.zhu0815@gmail.com>
2020-03-19 13:12:02 +1300

    New

--- Assignment1/Assignment1/urls.py
+++ Assignment1/Assignment1/urls.py
@@ -1,4 +1,4 @@
-"""WebDev_Assignment1 URL Configuration
+"""Assignment1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
---
+++ Assignment1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/teresa/Documents/GitHub/WebDev_Assignment1/Assignment1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
